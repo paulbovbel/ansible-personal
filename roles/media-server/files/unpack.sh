5e294f748ef93029932a3edfd395ad9b6228c74f
Paul Bovbel <paul@bovbel.com>
2019-08-18 22:23:43 -0400

    Fix rar unpack

--- roles/media-server/files/unpack.sh
+++ roles/media-server/files/unpack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ${TR_TORRENT_DIR}/${TR_TORRENT_NAME}
-find . -name '*.rar' -exec unrar e -o- {} \;
+find . -name '*.rar' -exec unrar x -o- {} \;
 find . -name '*.tar.gz' -exec tar -xvzf {} \;
 find . -name '*.zip' -exec unzip {} \;
