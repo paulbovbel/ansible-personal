dc5a802f7e9fadcf7361ce7276504f5a75250c5b
Paul Bovbel <paul@bovbel.com>
2020-11-06 14:17:54 -0500

    Switch to deluge

--- roles/media-server/files/unpack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ${TR_TORRENT_DIR}/${TR_TORRENT_NAME}
-find . -name '*.rar' -exec unrar x -o- {} \;
-find . -name '*.tar.gz' -exec tar -xvzf {} \;
-find . -name '*.zip' -exec unzip {} \;
