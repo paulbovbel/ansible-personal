02be75eaf8be796dfbb74385c698fd818ae9773d
Paul Bovbel <paul@bovbel.com>
2023-08-23 16:16:01 -0400

    Add port forward to tailscale

---
+++ roles/upnp/tasks/TODO.bash
@@ -0,0 +1,40 @@
+sudo apt-get install ufw
+
+sudo nano /etc/default/ufw IPV6=no
+# in /etc/ufw/after.rules, from https://github.com/chaifeng/ufw-docker
+# BEGIN UFW AND DOCKER
+*filter
+:ufw-user-forward - [0:0]
+:ufw-docker-logging-deny - [0:0]
+:DOCKER-USER - [0:0]
+-A DOCKER-USER -j ufw-user-forward
+
+-A DOCKER-USER -j RETURN -s 10.0.0.0/8
+-A DOCKER-USER -j RETURN -s 172.16.0.0/12
+-A DOCKER-USER -j RETURN -s 192.168.0.0/16
+
+-A DOCKER-USER -p udp -m udp --sport 53 --dport 1024:65535 -j RETURN
+
+-A DOCKER-USER -j ufw-docker-logging-deny -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -d 192.168.0.0/16
+-A DOCKER-USER -j ufw-docker-logging-deny -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -d 10.0.0.0/8
+-A DOCKER-USER -j ufw-docker-logging-deny -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -d 172.16.0.0/12
+-A DOCKER-USER -j ufw-docker-logging-deny -p udp -m udp --dport 0:32767 -d 192.168.0.0/16
+-A DOCKER-USER -j ufw-docker-logging-deny -p udp -m udp --dport 0:32767 -d 10.0.0.0/8
+-A DOCKER-USER -j ufw-docker-logging-deny -p udp -m udp --dport 0:32767 -d 172.16.0.0/12
+
+-A DOCKER-USER -j RETURN
+
+-A ufw-docker-logging-deny -m limit --limit 3/min --limit-burst 10 -j LOG --log-prefix "[UFW DOCKER BLOCK] "
+-A ufw-docker-logging-deny -j DROP
+
+COMMIT
+# END UFW AND DOCKER
+
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
+sudo ufw allow in on tailscale0 to any port 80 proto tcp
+sudo ufw allow in on tailscale0 to any port 443 proto tcp
+sudo ufw allow in on tailscale0 to any port 22 proto tcp
+sudo ufw allow 41641/udp
+sudo ufw route allow 32400/tcp
+sudo ufw enable
